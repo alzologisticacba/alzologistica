0be23d4c2a338cf1ecdc31c158ef6c2d770eff1e
alzologisticacba <alzologistica@gmail.com>
2026-02-23 13:46:55 -0300

    correccion de config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
 export default defineConfig({
   site: "https://alzologistica.com",
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: "static",
   integrations: [react()],
 });
