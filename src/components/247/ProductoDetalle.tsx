3fdb2856b169c6bbe57c33bb69680fa2a530df49
alzologisticacba <alzologistica@gmail.com>
2026-05-08 14:36:31 -0300

    cambios en compartir productos

--- src/components/247/ProductoDetalle.tsx
+++ src/components/247/ProductoDetalle.tsx
@@ -7,7 +7,7 @@ import Footer247 from "./Footer247";
 import PageFooterSection from "./PageFooterSection";
 import { addToCart } from "./hooks/cartStore";
 import { supabaseClient } from "../../lib/supabaseClient";
-import ShareButton from "./ShareButton";
+// import ShareButton from "./ShareButton";
 
 // const FONDOS_FIG = [
 //   "/img/247/figuritasFondo.webp",
@@ -264,10 +264,6 @@ export default function ProductoDetalle() {
                   <p className="pd__total">Subtotal: <strong>{fmt((precioConDescuento ?? articulo.precioFinal) * (parseInt(inputVal, 10) || cantidad))}</strong></p>
 
                   <BtnAgregar articulo={articulo} cantidad={cantidad} precioConDescuento={precioConDescuento} />
-                  {/* <ShareButton
-                    productName={articulo.descripcion}
-                    productUrl={`https://alzologistica.com/247/producto/?codigo=${articulo.codigo}`}
-                  /> */}
                 </div>
               </div>
             </div>
