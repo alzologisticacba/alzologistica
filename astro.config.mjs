22e7e9d9768499a75f34dad7489f67451540852a
alzologisticacba <alzologistica@gmail.com>
2026-05-08 12:53:43 -0300

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://alzologistica.com",
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
   integrations: [react()],
 });
