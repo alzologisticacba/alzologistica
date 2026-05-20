883992a5d3c37bec50c9cabc6936a9a302893d56
alzologisticacba <alzologistica@gmail.com>
2026-05-20 09:51:25 -0300

    fix detalle de productos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://alzologistica.com",
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
   integrations: [
     react(),
